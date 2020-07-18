a8eb06f209664521f012e7626c7159d963e8fa23
agustin <agustin@betterplan.cl>
2020-07-18 15:42:27 -0400

    imagenes para modelo producto

--- gestionCurricular/galeria/models.py
+++ gestionCurricular/galeria/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here
 class Product(models.Model):
     sku = models.SlugField(max_length=30, unique=True)
+    image = models.ImageField()
     name = models.CharField(max_length=255, unique=True)
     def __str__(self):
         return self.name
